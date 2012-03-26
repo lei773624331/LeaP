866f1d919a9592d35bb9e9b02ef8116db7ee406f
Lea <lea.zhanglei@gmail.com>
2012-03-26 17:07:42 +0200

    initial commit

---
+++ public/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
+	<head>
+		<!--
+    Created by Artisteer v3.0.0.35414
+    Base template (without user's data) checked by http://validator.w3.org : "This page is valid XHTML 1.0 Transitional"
+    -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
+		<title>About ME</title>
+		<link rel="stylesheet" href="./style.css" type="text/css" media="screen" />
+		<!--[if IE 6]><link rel="stylesheet" href="./style.ie6.css" type="text/css" media="screen" /><![endif]-->
+		<!--[if IE 7]><link rel="stylesheet" href="./style.ie7.css" type="text/css" media="screen" /><![endif]-->
+		<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+		<script type="text/javascript" src="./jquery.js"></script>
+		<script type="text/javascript" src="./script.js"></script>
+	</head>
+	<body>
+		<div id="main">
+			<div class="sheet">
+				<div class="sheet-cc"></div>
+				<div class="sheet-body">
+					<div class="header">
+						<div class="header-center">
+							<div class="header-jpeg"></div>
+						</div>
+						<div class="logo">
+							<h1 id="name-text" class="logo-name"><a href="./index.php">Zhang Lei</a></h1>
+							<h2 id="slogan-text" class="logo-text">do it or not do it there is no try</h2>
+						</div>
+					</div>
+					<div class="nav">
+						<div class="nav-l"></div>
+						<div class="nav-r"></div>
+						<ul class="menu">
+							<li><a href="./me.php" class="active"><span class="l"> </span><span class="r"> </span><span class="t">About ME</span></a></li>
+							<li><a href="./learning.php"><span class="l"> </span><span class="r"> </span><span class="t">BLOG</span></a></li>
+							<li><a href="./career.php"><span class="l"> </span><span class="r"> </span><span class="t">CV</span></a></li>
+						</ul>
+					</div>
+					<div class="content-layout">
+						<div class="content-layout-row">
+							<div class="layout-cell content">
+								<div class="post">
+									<div class="post-body">
+										<div class="post-inner article">
+											<h2 class="postheader">About ME</h2>
+											<div class="postcontent">
+												<p>Enter Page content here...</p>
+												<img src="./images/WP_000437.jpg" alt="an image" style="float: left; " width="424" height="238" />
+												<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. Cras elit nisl, rhoncus nec iaculis ultricies, feugiat eget sapien. Pellentesque ac felis tellus.</p>
+												<p>Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices. Phasellus elit dolor, porttitor id consectetur sit amet, posuere id magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+												<p> Suspendisse pharetra auctor pharetra. Nunc a sollicitudin est. Curabitur ullamcorper gravida felis, sit amet scelerisque lorem iaculis sed. Donec vel neque in neque porta venenatis sed sit amet lectus. Fusce ornare elit nisl, feugiat bibendum lorem. Vivamus pretium dictum sem vel laoreet. In fringilla pharetra purus, semper vulputate ligula cursus in. Donec at nunc nec dui laoreet porta eu eu ipsum. Sed eget lacus sit amet risus elementum dictum.</p>
+											</div>
+											<div class="cleared"></div>
+										</div>
+										<div class="cleared"></div>
+									</div>
+								</div>
+								<div class="cleared"></div>
+							</div>
+						</div>
+					</div>
+					<div class="cleared"></div>
+					<div class="footer">
+						<div class="footer-t"></div>
+						<div class="footer-body">
+							<div class="footer-text">
+								<p><a href="./#">Link1</a> | <a href="./#">Link2</a> | <a href="./#">Link3</a></p>
+								<p>Copyright © 2012. All Rights Reserved.</p>
+							</div>
+							<div class="cleared"></div>
+						</div>
+					</div>
+					<div class="cleared"></div>
+				</div>
+			</div>
+			<div class="cleared"></div>
+			<p class="page-footer">Designed by Lea Zhang Lei.</p>
+		</div>
+	</body>
+</html>
---
+++ public/learning.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
+	<head>
+		<!--
+    Created by Artisteer v3.0.0.35414
+    Base template (without user's data) checked by http://validator.w3.org : "This page is valid XHTML 1.0 Transitional"
+    -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
+		<title>BLOG</title>
+		<link rel="stylesheet" href="./style.css" type="text/css" media="screen" />
+		<!--[if IE 6]><link rel="stylesheet" href="./style.ie6.css" type="text/css" media="screen" /><![endif]-->
+		<!--[if IE 7]><link rel="stylesheet" href="./style.ie7.css" type="text/css" media="screen" /><![endif]-->
+		<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+		<script type="text/javascript" src="./jquery.js"></script>
+		<script type="text/javascript" src="./script.js"></script>
+	</head>
+	<body>
+		<div id="main">
+			<div class="sheet">
+				<div class="sheet-cc"></div>
+				<div class="sheet-body">
+					<div class="header">
+						<div class="header-center">
+							<div class="header-jpeg"></div>
+						</div>
+						<div class="logo">
+							<h1 id="name-text" class="logo-name"><a href="./index.php">Zhang Lei</a></h1>
+							<h2 id="slogan-text" class="logo-text">do it or not do it there is no try</h2>
+						</div>
+					</div>
+					<div class="nav">
+						<div class="nav-l"></div>
+						<div class="nav-r"></div>
+						<ul class="menu">
+							<li><a href="./me.php"><span class="l"> </span><span class="r"> </span><span class="t">About ME</span></a></li>
+							<li><a href="./learning.php" class="active"><span class="l"> </span><span class="r"> </span><span class="t">BLOG</span></a></li>
+							<li><a href="./career.php"><span class="l"> </span><span class="r"> </span><span class="t">CV</span></a></li>
+						</ul>
+					</div>
+					<div class="content-layout">
+						<div class="content-layout-row">
+							<div class="layout-cell content">
+								<div class="post">
+									<div class="post-body">
+										<div class="post-inner article">
+											<h2 class="postheader">BLOG</h2>
+											<div class="postcontent">
+												<p><img width="397" height="255" alt="" src="./images/Capture.jpg" /></p>
+											</div>
+											<div class="cleared"></div>
+										</div>
+										<div class="cleared"></div>
+									</div>
+								</div>
+								<div class="cleared"></div>
+							</div>
+						</div>
+					</div>
+					<div class="cleared"></div>
+					<div class="footer">
+						<div class="footer-t"></div>
+						<div class="footer-body">
+							<div class="footer-text">
+								<p><a href="./#">Link1</a> | <a href="./#">Link2</a> | <a href="./#">Link3</a></p>
+								<p>Copyright © 2012. All Rights Reserved.</p>
+							</div>
+							<div class="cleared"></div>
+						</div>
+					</div>
+					<div class="cleared"></div>
+				</div>
+			</div>
+			<div class="cleared"></div>
+			<p class="page-footer">Designed by Lea Zhang Lei.</p>
+		</div>
+	</body>
+</html>
