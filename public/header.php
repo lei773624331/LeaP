c8300a63595c827fd88ded2ec38d91dfea375b59
lei773624331 <lea.zhanglei@gmail.com>
2012-03-27 14:39:23 +0300

    léa, fix error db

--- public/header.php
+++ public/header.php
@@ -1,7 +1,8 @@
 <?php
-mysql_connect('localhost','root','');
-
-mysql_select_db('diary');
+/* Connexion database
+  mysql_connect('localhost','root','');
+  mysql_select_db('diary');
+  */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US" xml:lang="en">
